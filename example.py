006af17f3ffde6c706921fbda85ea3afd303e45a
“Anton <a.shiryaev@cashalot.mcfef.ru>
2025-03-25 18:20:05 +1000

    docs(README.md): Написал подробный README.md
    Написал подробный README.md

--- example.py
+++ example.py
@@ -1,5 +1,6 @@
+from typing import Any, Dict
+
 from model_interface.model_factory import ModelFactory
-from typing import Dict, Any
 
 
 def main() -> None:
@@ -9,8 +10,8 @@ def main() -> None:
     и выводит информацию о них.
     """
     # Имена моделей и семейство моделей
-    model_name_1 = "Qwen2-VL-2B"
-    model_name_2 = "Qwen2-VL-7B"
+    model_name_1 = "Qwen2-VL-2B-Instruct"
+    model_name_2 = "Qwen2-VL-7B-Instruct"
     model_family = "Qwen2-VL"
 
     # Регистрация модели в фабрике
@@ -21,18 +22,18 @@ def main() -> None:
     # Параметры для инициализации первой модели
     model_init_params_1: Dict[str, Any] = {
         "model_name": model_name_1,
-        "system_prompt": "",
+        "system_prompt": "path_to_system_prompt_1.txt",
         "cache_dir": "cache",
     }
 
     # Параметры для инициализации второй модели
     model_init_params_2: Dict[str, Any] = {
         "model_name": model_name_2,
-        "system_prompt": "",
+        "system_prompt": "path_to_system_prompt_2.txt",
         "cache_dir": "cache",
     }
 
-    # Создание экземпляров моделей
+    # Создание объектов моделей
     model_1 = ModelFactory.get_model(
         model_family, model_init_params=model_init_params_1
     )
@@ -43,10 +44,18 @@ def main() -> None:
     # Вывод информации о моделях
     print(f"Создана Модель 1: {model_1}")
     model_1.print()
+    dummy_result_1 = model_1.predict_on_image(
+        image="path_to_image_1.jpg", question="Что изображено на картинке?"
+    )
+    print(f"Псевдо ответ от модели 1: {dummy_result_1}")
 
     print()
     print(f"Создана Модель 2: {model_2}")
     model_2.print()
+    dummy_result_2 = model_2.predict_on_image(
+        image="path-to_image_2.jpg", question="Что изображено на картинке?"
+    )
+    print(f"Псевдо ответ от модели 2: {dummy_result_2}")
 
 
 if __name__ == "__main__":
